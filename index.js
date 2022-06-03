696a7e43d6aabac7860a333ef2e824223a3e840e
Romeo-ag <romeo.a.gurning@gmail.com>
2022-06-03 19:07:57 -0400

    Menambahkan awalan strip pada listBelanjaan

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const dataBelanjaan = [
 const listBelanjaan = (dataBelanjaan) => {
   let outData = [];
 for(i=0;i<dataBelanjaan.length;i++){
-outData.push(dataBelanjaan[i].nama + " x " + dataBelanjaan[i].kuantitas);
+outData.push("- " + dataBelanjaan[i].nama + " x " + dataBelanjaan[i].kuantitas);
 }
 return outData;
 };
